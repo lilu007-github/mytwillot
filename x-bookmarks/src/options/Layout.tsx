1a409571eb4305c973166205ec1d8754b186d099
lilu007-github <lilu007_snow@hotmail.com>
2026-05-31 14:05:55 -0700

    fix: remove Export link from sidebar navigation

--- x-bookmarks/src/options/Layout.tsx
+++ x-bookmarks/src/options/Layout.tsx
@@ -20,8 +20,6 @@ import { Alert } from '../components/Alert'
 import Notification from '../components/Notification'
 import {
   IconBookmark,
-  IconCrown,
-  IconExport,
   IconFolderMove,
   IconFolders,
   IconMessage,
@@ -38,12 +36,10 @@ import AsideFolder from '../components/AsideFolder'
 import { getCurrentUserId, onLocalChanged, StorageKeys } from 'utils/storage'
 import { getLicense, isViolatedLicense, LICENSE_KEY } from 'utils/license'
 import Spinner from '~/components/Spinner'
-import { PRICING_URL } from '~/libs/member'
 
 export const Layout = (props) => {
   const [store, setStore] = dataStore
   const [searchParams] = useSearchParams()
-  const isPremium = () => store[LICENSE_KEY] && store[LICENSE_KEY].level > 0
   const [bookmarksOpen, setBookmarksOpen] = createSignal(true)
   const [foldersOpen, setFoldersOpen] = createSignal(true)
 
@@ -293,20 +289,6 @@ export const Layout = (props) => {
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
                   </svg>
                   <span class="ms-3 flex-1 whitespace-nowrap">Users</span>
-                </A>
-              </li>
-              <li>
-                <A
-                  class="cursor-d flex items-center rounded-lg p-2  hover:bg-gray-100 dark:hover:bg-gray-700"
-                  href="/export"
-                >
-                  <IconExport />
-                  <span class="ms-3 flex-1 whitespace-nowrap">Export</span>
-                  <span
-                    class={`ms-3 inline-flex scale-75 items-center justify-center rounded-full text-xs ${isPremium() ? 'text-yellow-400' : 'text-gray-500'}`}
-                  >
-                    <IconCrown />
-                  </span>
                 </A>
               </li>
             </ul>
